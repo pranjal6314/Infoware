61fdad3cd0366275d0c4a52fdade47ed407c8623
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2023-04-11 22:51:09 +0530

    importing required libraries and modules

--- app.js
+++ app.js
@@ -16,3 +16,12 @@
 //   contact_info VARCHAR(255) NOT NULL,
 //   FOREIGN KEY (employee_id) REFERENCES employees(id) ON DELETE CASCADE
 // );
+const express = require("express");
+const app = express();
+const port = 4000;
+app.use(express.json({ limit: `100mb` }));
+const util = require("util");
+
+const mysql = require("mysql2");
+
+const connection = require("./db");
